99e81cb0772315592f7fc0da720034caba2799ed
Haiyang Sun <samtoo2006@gmail.com>
2022-06-23 12:17:17 -0700

    Initial commit

---
+++ ApplicationCore/Contracts/Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Contracts.Repositories
+{
+    public interface IMovieRepository
+    {
+        // all the database logic method for movies
+
+        //GetMovie(int id)
+        //MovieModel GetMovieDetails(int movieId)
+    }
+}
---
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+using System.Diagnostics;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopRatedMovies()
+        {
+            /*call the movie service
+             * movie serive will call movie repository
+             * movie repository will call the database to get the data
+             */
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
